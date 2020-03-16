51ae3bd4f5500c7ec01f3b64fa7fdc5e568b0ff0
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-03-17 06:25:23 +1100

    Minor amends to bash_aliases

--- playbook/bash/files/bash_aliases.sh
+++ playbook/bash/files/bash_aliases.sh
@@ -4,5 +4,6 @@ alias ap='ansible-playbook'
 
 alias dcp='docker-compose'
 alias dps='docker ps'
-alias dstop='docker stop $(docker ps -a -q)'
-alias drm='docker rm $(docker ps -a -q)'
+alias dstop='{ docker stop $(docker ps -a -q) 2>/dev/null; } || { echo "No containers running"; }'
+alias drm='{ docker rm $(docker ps -a -q) 2>/dev/null; } || { echo "No containers to remove"; }'
+alias ddwn='dstop && drm'
