5310a2a9e46447a28df4fd8dc7497048b1774bf3
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-03-17 05:46:23 +1100

    Adds bash_aliases

---
+++ playbook/bash/files/bash_aliases.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+alias ap='ansible-playbook'
+
+alias dcp='docker-compose'
+alias dps='docker ps'
+alias dstop='docker stop $(docker ps -a -q)'
+alias drm='docker rm $(docker ps -a -q)'
