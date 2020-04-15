7f90db677a2742e604485e8d17d049d7b6e8998e
Dmitrii Tcimbal <dtsimbal@gmail.com>
2020-04-15 21:59:41 +1000

    Adds bash utility function to reduce pdf size

--- playbook/bash/files/bash_functions.sh
+++ playbook/bash/files/bash_functions.sh
@@ -6,3 +6,11 @@ venv() {
    fi
    source .env/bin/activate
 }
+
+pdf-reduce() {
+    if [[ ! -f "$1" ]]; then
+        echo "Cannot find file '$1'"
+    else
+        gs -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -sOutputFile="$2" "$1"
+    fi
+}
