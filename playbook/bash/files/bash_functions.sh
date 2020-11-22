0700eb2a9b87c9c2878e4be83dbbb7eeff0dbdf4
Dmitrii Tcimbal <dtcimbal@gmail.com>
2020-11-22 15:54:15 +1100

    Adds project opening function

--- playbook/bash/files/bash_functions.sh
+++ playbook/bash/files/bash_functions.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+export CODE_DIR="${HOME}/code"
+
 venv() {
    if [[ ! -d .venv ]]; then
         python3 -m venv .venv
@@ -21,4 +23,34 @@ g() {
 
 frm() {
   find . -name $1 -exec rm -rf {} \;
+}
+
+p() {
+    if [[ -z $1 ]]; then
+        ls "${CODE_DIR}";
+        return 0;
+    fi;
+
+    declare -a matching_dir_array=();
+
+    while IFS= read -rd '' matching_dir; do
+        matching_dir_array+=("$matching_dir");
+    done < <(find ${CODE_DIR} -maxdepth 1 -type d -iname "$1*" -print0);
+    
+    matching_dir_number=${#matching_dir_array[@]};
+
+    if [[ "${matching_dir_number}" -eq 1 ]]; then
+        code "${matching_dir_array[0]}" && echo "${matching_dir_array[0]}";
+        return 0;
+    fi
+
+    if [[ "${matching_dir_number}" -gt 1 ]]; then
+        for matching_dir in "${matching_dir_array[@]}"
+        do
+            echo "${matching_dir}";
+        done;
+        return 0;
+    fi;
+
+    echo "no project dir $1";
 }
